cf4d3362a549a00530b498056a7414880251be14
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-03-04 13:15:44 +0000

    [LeetCode Sync] Runtime - 2 ms (6.15%), Memory - 42.1 MB (90.20%)

--- my-folder/0088-merge-sorted-array/solution.java
+++ my-folder/0088-merge-sorted-array/solution.java
@@ -1,19 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-
-       while(i >= 0 && j >=0) {
-           if (nums1[i] > nums2[j]) {
-               nums1[k--] = nums1[i--]; 
-           }
-           else
-                nums1[k--] = nums2[j--];
-       }
-
-       while(j >= 0) {
-           nums1[k--] = nums2[j--];
-       }
+        for (int i = 0; i < m; i++) {
+            nums1[i] = nums1[i];
+        }
+        for (int j = 0; j < n; j++) {
+            nums1[m+j] = nums2[j];
+        }
+        int temp;
+        for (int k = 0; k < m+n; k++) {
+            for (int a = k+1; a < m+n; a++) {
+                if (nums1[k] > nums1[a]) {
+                    temp = nums1[k];
+                    nums1[k] = nums1[a];
+                    nums1[a] = temp;
+                }
+            }
+        }
     }
 }
