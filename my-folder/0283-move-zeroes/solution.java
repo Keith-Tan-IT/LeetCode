cb7e0a5bfe60d9de43a7c89e4bd03b94f3997358
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-03-06 13:02:42 +0000

    [LeetCode Sync] Runtime - 2 ms (79.41%), Memory - 46 MB (87.45%)

--- my-folder/0283-move-zeroes/solution.java
+++ my-folder/0283-move-zeroes/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int counter = 0, current = 0;
-        while (current < nums.length) {
-            if (nums[current] == 0) {
-                counter += 1;
-            } else {
-                nums[current - counter] = nums[current];
-            } 
-            current++;
-        }
-        int zero_num = nums.length - counter;
-        for (int j = zero_num; j < nums.length; j++) {
-            nums[j] = 0;
+        int left = 0, temp;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
         }
     }
 }
