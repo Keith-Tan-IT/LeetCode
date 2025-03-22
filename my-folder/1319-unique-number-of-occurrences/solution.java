700a096a4de94bf8c3a4a0bd03c529b8d5edae78
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-03-22 11:39:20 +0000

    [LeetCode Sync] Runtime - 2 ms (98.28%), Memory - 42.1 MB (32.62%)

--- my-folder/1319-unique-number-of-occurrences/solution.java
+++ my-folder/1319-unique-number-of-occurrences/solution.java
@@ -2,7 +2,11 @@ class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         HashMap <Integer, Integer> count = new HashMap <>();
         for (int i = 0; i < arr.length; i++) {
-            count.put(arr[i], count.getOrDefault(arr[i], 0)+ 1);
+            if(!count.containsKey(arr[i]))
+                count.put(arr[i], 1);
+            else {
+                count.put(arr[i],count.get(arr[i]) + 1);
+            }
         }
         HashSet <Integer> set = new HashSet<>();
         for (int nums : count.values()) {
@@ -10,6 +14,7 @@ class Solution {
         }
 
         return count.size() == set.size();
+
         
     }
 }
