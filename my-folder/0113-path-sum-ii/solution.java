a4de5717f0fe70f0d2cdf2e96e4a7d1f978c3c88
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-04-16 13:22:15 +0000

    [LeetCode Sync] Runtime - 90 ms (3.71%), Memory - 45.7 MB (8.34%)

--- my-folder/0113-path-sum-ii/solution.java
+++ my-folder/0113-path-sum-ii/solution.java
@@ -15,30 +15,37 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List < List <Integer>> result = new LinkedList<>();
-        List<Integer> path = new LinkedList<>();
-        dfs(root, targetSum, path, result);
+        List<Integer> path = new LinkedList <>();
+        List<List<Integer>> result = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        TreeNode prev = null;
+        int sum = 0;
+        while (curr != null || !stack.empty()) {
+            while (curr != null) {
+                stack.push(curr);
+                path.add(curr.val);
+                sum += curr.val;
+                curr = curr.left;
+                System.out.println(path);
+            }
+            curr = stack.peek();
+            if (curr.right != null && curr.right != prev) {
+                curr = curr.right;
+                continue;
+            }
+            if (curr.left == null && curr.right == null) {
+                if (sum == targetSum) {
+                    result.add(new LinkedList(path));
+                }
+            }
+            System.out.println(path);
+            stack.pop();
+            path.removeLast();
+            sum -= curr.val;
+            prev = curr;
+            curr = null;
+        }
         return result;
-    }
-
-    public void dfs (TreeNode root, int targetSum, List path, List <List<Integer>> result) {
-        if (root == null) {
-            return;
-        }
-        path.add(root.val);
-        if (root.right == null && root.left == null) {
-            if (root.val == targetSum) {
-                result.add(new LinkedList(path));
-            }
-        }
-        else {
-            if (root.left != null) {
-                dfs(root.left, targetSum - root.val, path, result);
-            }
-            if (root.right != null) {
-                dfs(root.right, targetSum - root.val, path, result);
-            }    
-        }
-        path.remove(path.size()-1);
     }
 }
