a1bee60fc86fe7592ac0a32f8c9accd225799a7f
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-04-22 15:51:03 +0000

    [LeetCode Sync] Runtime - 4 ms (33.82%), Memory - 42.3 MB (38.77%)

--- my-folder/0017-letter-combinations-of-a-phone-number/solution.java
+++ my-folder/0017-letter-combinations-of-a-phone-number/solution.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> result = new LinkedList<>();
-        String[] map = new String [] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        if (digits.isEmpty()) {
-            return result; 
+        List<String> result = new ArrayList<>();
+        if (digits.length() == 0 || digits == null) {
+            return result;
         }
-        result.add(0, "");
-        for (int i = 0; i < digits.length(); i++) {
-            int a = Character.getNumericValue(digits.charAt(i));
-            int k = result.size();
-            for (int j = 0; j < k; j++) {
-                String firstLetter = result.remove(0);
-                for (char letter : map[a].toCharArray()) {
-                    result.add(firstLetter + letter);
-                }
+        String map[] = new String[] {"0", "1", "abc", "def" , "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        backTracking(digits, "", map, result);
+        return result;
+
+    }
+
+    public void backTracking(String next_digits, String combination, String[] map, List<String> result) {
+        if (next_digits.isEmpty()) {
+            result.add(combination);
+        }
+        else {
+            String letters = map[next_digits.charAt(0) - '0'];
+            for (char letter : letters.toCharArray()) {
+                backTracking(next_digits.substring(1), combination + letter, map, result);
             }
         }
-        return result;
     }
 }
