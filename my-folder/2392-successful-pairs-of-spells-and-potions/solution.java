7f9534db46ccda9fe3be397e0a32a1912c253c58
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-05-10 16:00:39 +0000

    [LeetCode Sync] Runtime - 111 ms (5.08%), Memory - 67.9 MB (5.71%)

--- my-folder/2392-successful-pairs-of-spells-and-potions/solution.java
+++ my-folder/2392-successful-pairs-of-spells-and-potions/solution.java
@@ -1,21 +1,15 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        TreeMap <Long, Integer> map = new TreeMap<>();
         Arrays.sort(potions);
-        int[] result = new int[spells.length];  
-        for (int i = 0; i < spells.length; i++) {
-            int left = 0;
-            int right = potions.length - 1;
-            while (right >= left) {
-                int mid = (right + left )/ 2;
-                if ((long) potions[mid] * spells[i] < success) {
-                    left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
-            }
-            result[i] = potions.length - left;
+        map.put(Long.MAX_VALUE,potions.length);
+        for (int i = potions.length - 1; i >=0; i--) {
+            map.put((long) potions[i], i);
         }
-        return result;
+        for (int i = 0; i < spells.length; i++) {
+            long need = (success + spells[i] - 1 ) / spells[i];
+            spells[i] = potions.length - map.ceilingEntry(need).getValue();
+        }
+        return spells;
     }
 }
