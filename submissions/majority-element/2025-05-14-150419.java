9370c168e72a46ee15692af4121750a423ad3a18
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-05-14 15:04:19 +0000

    LeetCode submission: majority-element

---
+++ submissions/majority-element/2025-05-14-150419.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int counter = 0, candidate = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if(counter == 0) {
+                candidate = nums[i];
+            }
+            if (nums[i] == candidate) {
+                counter++;
+            }
+            else {
+                counter--;
+            }
+        }
+        return candidate;
+    }
+}
