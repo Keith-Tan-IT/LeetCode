eda42adb498ea8b49e8de0d614e1676a8e43faba
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-05-14 14:25:44 +0000

    LeetCode submission: koko-eating-bananas

---
+++ submissions/koko-eating-bananas/2025-05-14-142544.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        int left = 1;
+        for (int i = 0; i < piles.length; i++) {
+            max = Math.max(max, piles[i]);
+        }
+        int right = max;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            long counter = 0;
+            for (int i = 0; i < piles.length; i++) {
+                counter += piles[i] / mid;
+                if (piles[i] % mid != 0) {
+                    counter++;
+                }
+            }
+            if (counter <= h) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
