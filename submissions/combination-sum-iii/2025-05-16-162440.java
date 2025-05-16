2af52323a5dc5a5b8db4377fa0dd49922b47ac23
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-05-16 16:24:40 +0000

    LeetCode submission: combination-sum-iii

---
+++ submissions/combination-sum-iii/2025-05-16-162440.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        backTracking(k, n, result, new ArrayList<>(), 1);
+        return result;
+    }
+    public void backTracking(int k, int n, List<List<Integer>> result, List<Integer> combination, int start) {
+        if (combination.size() > k) {
+            return;
+        }
+        if (combination.size() == k && n == 0) {
+            List<Integer> newList = new ArrayList<>(combination); 
+            result.add(newList);
+        }
+        for (int i = start; i <= 9; i++) {
+            combination.add(i);
+            backTracking(k, n - i, result, combination, i + 1);
+            combination.remove((Integer) i);
+        }
+    }
+}
