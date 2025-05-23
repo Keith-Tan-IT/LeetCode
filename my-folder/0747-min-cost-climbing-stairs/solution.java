ad705e0ea51e6686f33c7b78333939828245e21a
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-05-23 14:28:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.7 MB (10.66%)

--- my-folder/0747-min-cost-climbing-stairs/solution.java
+++ my-folder/0747-min-cost-climbing-stairs/solution.java
@@ -1,13 +1,11 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        // Reduce from O(n) to O(1) space
-        int first = cost[0];
-        int second = cost[1];
-        for (int i = 2; i < cost.length; i++) {
-            int curr = cost[i] + Math.min(first, second);
-            first = second;
-            second = curr;
+        int[] dp = new int [cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
-        return Math.min(first, second);
+        return dp[cost.length];
     }
 }
