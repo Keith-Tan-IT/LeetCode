3cf4157bcfba85d91350605745ff6ce1885a30a7
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-06-10 12:50:25 +0000

    [LeetCode Sync] Runtime - 1 ms (6.73%), Memory - 43.8 MB (5.57%)

---
+++ my-folder/0062-unique-paths/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    int[][] dp = new int [101][101];
+    public int uniquePaths(int m, int n) {
+        return dfs(m, n, 0, 0);
+    }
+    public int dfs(int m, int n, int i, int j) {
+        if (i >= m || j >= n) {
+            return 0;
+        }
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        dp[i][j] = dfs(m, n, i + 1, j) + dfs(m, n, i, j+1);
+        return dp[i][j];
+    }
+}
