b163c1e7af4e56d3aae1aaa40dcc3118f7387032
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-06-11 14:36:42 +0000

    [LeetCode Sync] Runtime - 1 ms (99.86%), Memory - 61.5 MB (76.23%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.java
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,15 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int max = 0;
-        if (prices.length == 1) {
-            return 0;
-        }
-        int cheapest = Integer.MAX_VALUE;
+        int buy = prices[0];
         for (int i = 0; i < prices.length; i++) {
-            cheapest = Math.min(prices[i], cheapest);
-            if (i > 0 && prices[i] > cheapest) {
-                max = Math.max(max, prices[i] - cheapest);
+            if (prices[i] < buy) {
+                buy = prices[i];
             }
+            max = Math.max(max, prices[i] - buy);
         }
         return max;
     }
