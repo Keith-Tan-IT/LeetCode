b25e1859adba1c50122ff11e3a3696c5a30f13d8
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-06-17 13:50:00 +0000

    [LeetCode Sync] Runtime - 205 ms (5.13%), Memory - 94.9 MB (5.02%)

--- my-folder/0123-best-time-to-buy-and-sell-stock-iii/solution.java
+++ my-folder/0123-best-time-to-buy-and-sell-stock-iii/solution.java
@@ -1,15 +1,27 @@
 class Solution {
+    Integer[][][] memo;
     public int maxProfit(int[] prices) {
-        int buy1 = Integer.MIN_VALUE;
-        int sell1 = 0;
-        int buy2 = Integer.MIN_VALUE;
-        int sell2 = 0;
-        for (int i = 0; i < prices.length; i++) {
-            buy1 = Math.max(buy1, -prices[i]);
-            sell1 = Math.max(sell1, buy1 + prices[i]);
-            buy2 = Math.max(buy2, sell1 - prices[i]);
-            sell2 = Math.max(sell2, buy2 + prices[i]);
+        int n = prices.length;
+        memo = new Integer[n][2][3];
+        return dfs(prices, 0, 1, 2);
+    }
+    public int dfs(int[] prices, int index, int canBuy, int transactionLeft) {
+        if (index == prices.length || transactionLeft == 0) {
+            return 0;
         }
-        return sell2;
+        if (memo[index][canBuy][transactionLeft] != null) {
+            return memo[index][canBuy][transactionLeft];
+        }
+        if (canBuy == 1) {
+            int buy = -prices[index] + dfs(prices, index + 1, 0, transactionLeft);
+            int skip = dfs(prices, index + 1, 1, transactionLeft);
+            memo[index][canBuy][transactionLeft] = Math.max(buy, skip);
+        }
+        else {
+            int sell = prices[index] + dfs(prices, index + 1, 1, transactionLeft - 1);
+            int skip = dfs(prices, index + 1, 0, transactionLeft);
+            memo[index][canBuy][transactionLeft] = Math.max(sell, skip);
+        }
+        return memo[index][canBuy][transactionLeft];
     }
 }
