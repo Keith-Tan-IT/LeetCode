eb1bf5ab227ed6698e2d777e91ab1adae5214a25
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-07-02 14:24:56 +0000

    [LeetCode Sync] Runtime - 5 ms (59.24%), Memory - 44.7 MB (71.18%)

--- my-folder/0013-roman-to-integer/solution.java
+++ my-folder/0013-roman-to-integer/solution.java
@@ -2,6 +2,7 @@ class Solution {
     public int romanToInt(String s) {
         Map <Character, Integer> map = new HashMap<>();
         int ans = 0;
+        int prev = 0;
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -10,13 +11,14 @@ class Solution {
         map.put('D', 500);
         map.put('M', 1000);
 
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
-                ans -= map.get(s.charAt(i));
-            }
-            else {
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (map.get(s.charAt(i)) >= prev) {
                 ans += map.get(s.charAt(i));
             }
+            else {
+                ans -= map.get(s.charAt(i));
+            }
+            prev = map.get(s.charAt(i));
         }
         return ans;
     }
