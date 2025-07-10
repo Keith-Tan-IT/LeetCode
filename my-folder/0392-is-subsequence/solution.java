0d799948fd8773ffc09abcbe74020041438ee77c
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-07-10 14:01:33 +0000

    [LeetCode Sync] Runtime - 1 ms (92.74%), Memory - 42.5 MB (5.23%)

--- my-folder/0392-is-subsequence/solution.java
+++ my-folder/0392-is-subsequence/solution.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int j = 0;
-        if (s.length() == 0) {
+        return dfs(s, t, s.length(), t.length());
+    }
+    
+    public boolean dfs (String s, String t, int i, int j) {
+        if (i == 0) {
             return true;
         }
-        for (int i = 0; i < t.length(); i++) {
-            if (t.charAt(i) == s.charAt(j)) {
-                j += 1;                  
-                if (j == s.length()) {
-                        return true;
-                }
-            }
+        if (j == 0) {
+            return false;
         }
-        return false;
+        if (s.charAt(i - 1) == t.charAt(j - 1)) {
+            return dfs(s, t, i - 1, j - 1);
+        }
+        else {
+            return dfs(s, t, i, j - 1);
+        }
     }
 }
