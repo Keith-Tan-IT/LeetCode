aeb247b3c940cc2c94d7a287b543a0bd7e65c448
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-07-15 12:45:09 +0000

    [LeetCode Sync] Runtime - 1 ms (86.91%), Memory - 48.2 MB (28.51%)

--- my-folder/0547-number-of-provinces/solution.java
+++ my-folder/0547-number-of-provinces/solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int provinceCount = 0;
-        boolean[] visited = new boolean[isConnected.length];
+        int circleNum = 0;
+        int[] visited = new int[isConnected.length];
         for (int i = 0; i < isConnected.length; i++) {
-            if (!visited[i]) {
-                dfs(i, isConnected, visited);
-                provinceCount++;
+            if (visited[i] == 0) {
+                dfs(isConnected, visited, i);
+                circleNum++;
             }
         }
-        return provinceCount;
+        return circleNum;
     }
-    public void dfs (int i, int[][] isConnected, boolean[] visited) {
-        visited[i] = true;
+
+    public void dfs(int[][] isConnected, int[] visited, int i) {
         for (int j = 0; j < isConnected.length; j++) {
-            if (isConnected[i][j] == 1 && !visited[j]) {
-                dfs(j, isConnected, visited);
+            if (isConnected[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(isConnected, visited, j);
             }
         }
     }
