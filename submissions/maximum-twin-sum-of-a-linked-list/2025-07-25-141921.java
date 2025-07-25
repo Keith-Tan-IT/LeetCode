b4b143d0ad6bf59dc738e1d2e13e46955917c1a3
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-07-25 14:19:21 +0000

    LeetCode submission: maximum-twin-sum-of-a-linked-list

---
+++ submissions/maximum-twin-sum-of-a-linked-list/2025-07-25-141921.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head, fast = head, secondHalf, prev = null;
+
+        while (fast != null) {
+            fast = fast.next.next;
+            secondHalf = slow.next;
+            slow.next = prev;
+            prev = slow; 
+            slow = secondHalf;
+        }
+
+        int max = 0;
+        while (prev != null) {
+            max = Math.max(slow.val + prev.val, max);
+            prev = prev.next;
+            slow = slow.next;
+        }
+        return max;
+    }
+}
+// 0 1 2 - 3 4 5 
