7d48af083983f9cb09157521e938dea6cb4bc3bc
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-07-27 14:01:47 +0000

    [LeetCode Sync] Runtime - 7 ms (61.71%), Memory - 44.6 MB (74.63%)

--- my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.java
@@ -12,16 +12,12 @@ class Solution {
         if (root == null || root == p || root == q) {
             return root;
         }
-        TreeNode left = lowestCommonAncestor (root.left, p, q);
-        TreeNode right = lowestCommonAncestor (root.right, p, q);
-        if (left != null && right != null) {
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null & right != null) {
             return root;
         }
-        else if (left != null) {
-            return left;
-        }
-        else {
-            return right;
-        }
+        return left != null ? left : right;
     }
 }
