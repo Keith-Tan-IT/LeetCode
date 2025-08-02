5a4ce46ca4fe432001fda5f07af8188b15226c43
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-08-02 13:23:22 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 44.6 MB (40.45%)

--- my-folder/0002-add-two-numbers/solution.java
+++ my-folder/0002-add-two-numbers/solution.java
@@ -10,36 +10,26 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int sum = 0, carry = 0;
-        ListNode result = new ListNode();
-        ListNode dummy = result;
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        int carry = 0;
+
         while (l1 != null || l2 != null || carry != 0) {
-            if (l1 != null && l2 != null) {
-                sum = (l1.val + l2.val + carry) % 10;
-                carry = (l1.val + l2.val + carry) / 10;
-            }
-            else if (l1 != null) {
-                sum = (l1.val + carry) % 10;
-                carry = (l1.val + carry) / 10;
-            }
-            else if (l2 != null ) {
-                sum = (l2.val + carry) % 10;
-                carry = (l2.val + carry) / 10;
-            }
-            else {
-                sum = carry;
-                carry = 0;
-            }
-            ListNode curr = new ListNode(sum);
-            result.next = curr;
-            result = result.next;
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
+            int digit1 = (l1 == null) ? 0 : l1.val;
+            int digit2 = (l2 == null) ? 0 : l2.val;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode result = new ListNode();
+            result.val = digit;
+            tail.next = result;
+            tail = tail.next;
+
+            l1 = (l1 == null) ? null : l1.next;
+            l2 = (l2 == null) ? null : l2.next; 
         }
-        return dummy.next;
+        return dummyHead.next;
     }
 }
