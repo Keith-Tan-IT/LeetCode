0873c42af83742613d52f5d39d80eed7be0ed13d
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-08-02 13:23:22 +0000

    LeetCode submission: add-two-numbers

---
+++ submissions/add-two-numbers/2025-08-02-132322.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 == null) ? 0 : l1.val;
+            int digit2 = (l2 == null) ? 0 : l2.val;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            ListNode result = new ListNode();
+            result.val = digit;
+            tail.next = result;
+            tail = tail.next;
+
+            l1 = (l1 == null) ? null : l1.next;
+            l2 = (l2 == null) ? null : l2.next; 
+        }
+        return dummyHead.next;
+    }
+}
