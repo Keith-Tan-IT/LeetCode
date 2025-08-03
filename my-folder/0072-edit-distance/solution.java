77ad20d60dba83370ec34705e32d99644b276e44
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-08-03 15:40:54 +0000

    [LeetCode Sync] Runtime - 5 ms (57.46%), Memory - 44.9 MB (79.02%)

--- my-folder/0072-edit-distance/solution.java
+++ my-folder/0072-edit-distance/solution.java
@@ -1,24 +1,26 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = i;
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        /*for (int[] rows : dp) {
+            Arrays.fill(rows, 500);
         }
-        for (int j = 0; j <= m; j++) {
-            dp[0][j] = j;
+        dp[word1.length()][word2.length()] = 0;*/
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][word2.length()] = word1.length() - i;
         }
-        for (int i = 1; i <= n; i ++) {
-            for (int j = 1; j <= m; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
+        for (int j = 0; j < word2.length(); j++) {
+            dp[word1.length()][j] = word2.length() - j;
+        }
+        for (int i = word1.length() - 1; i >= 0; i--) {
+            for (int j = word2.length() - 1; j >= 0; j--) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
                 }
                 else {
-                    dp[i][j] =  1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    dp[i][j] = 1 +  Math.min(dp[i + 1][j], Math.min(dp[i + 1][j + 1], dp[i][j + 1]));
                 }
             }
         }
-        return dp[n][m];
+        return dp[0][0];
     }
 }
