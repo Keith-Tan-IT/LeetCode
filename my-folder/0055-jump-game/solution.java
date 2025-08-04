347f1d74677a117691ea2f47161b334421aded20
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-08-04 14:16:31 +0000

    [LeetCode Sync] Runtime - 3 ms (38.52%), Memory - 45.6 MB (85.56%)

--- my-folder/0055-jump-game/solution.java
+++ my-folder/0055-jump-game/solution.java
@@ -1,11 +1,36 @@
+/* memoization to avoid StackOverflow
 class Solution {
     public boolean canJump(int[] nums) {
-        int fast = 0;
+        Boolean[] memo = new Boolean[nums.length];
+        return dfs(nums, 0, memo);
+    }
+    public boolean dfs (int[] nums, int furthest, Boolean[] memo) {
+        if (memo[furthest] != null) {
+            return memo[furthest];
+        }
+        if (furthest >= nums.length - 1) {
+            return true;
+        }
+        for (int i = 1; i <= nums[furthest]; i++) {
+            if (dfs(nums, furthest + i, memo) == true) {
+                memo[furthest] = true;
+                return memo[furthest];
+            }
+        }
+        memo[furthest] = false;
+        return memo[furthest];
+    }
+}
+*/
+
+class Solution {
+    public boolean canJump (int[] nums) {
+        int reach = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i > fast) {
+            if (i > reach) {
                 return false;
             }
-            fast = Math.max(fast, nums[i] + i);
+            reach = Math.max(reach, i + nums[i]);
         }
         return true;
     }
