733d9318699f192462a81017609648c2e17ed6b0
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-08-13 14:44:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (19.93%)

--- my-folder/0274-h-index/solution.java
+++ my-folder/0274-h-index/solution.java
@@ -1,11 +1,23 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] >= citations.length - i) {
-                return citations.length - i;
+        int n = citations.length;
+        int bucket[] = new int[n + 1];
+        for (int c : citations) {
+            if (c >= n) {
+                bucket[n]++;
+            }
+            else {
+                bucket[c]++;
             }
         }
-        return 0;
+
+        int count = 0;
+        for (int i = n; i >= 0; i--) {
+            count += bucket[i];
+            if (count >= i) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
