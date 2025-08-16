ac7a7996f577ee72b7f30d4350ea81065d49bcba
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-08-16 13:49:07 +0000

    [LeetCode Sync] Runtime - 4 ms (100.00%), Memory - 44.1 MB (46.77%)

--- my-folder/0009-palindrome-number/solution.java
+++ my-folder/0009-palindrome-number/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || x % 10 == 0 && x != 0) {
             return false;
         }
-        long reverse = 0;
-        int ori = x;
-        int temp;
-        while (x > 0) {
-            temp = x % 10;
-            reverse += temp;
-            reverse *= 10;
+        int reverseHalf = 0;
+        while (x > reverseHalf) {
+            reverseHalf = reverseHalf * 10 + x % 10;
             x /= 10;
         }
-        return reverse / 10 == ori;
+        return x == reverseHalf || x == reverseHalf / 10;
     }
 }
