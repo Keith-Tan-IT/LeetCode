958913d408ed0133f6486727f7a51c53fb633a41
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-08-23 15:49:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.2 MB (19.74%)

--- my-folder/0066-plus-one/solution.java
+++ my-folder/0066-plus-one/solution.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int carry = 1;
-        for (int i = digits.length - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
-            digits[i] = sum % 10;
-            carry = sum / 10;
+        for (int i = digits.length - 1 ; i >= 0 ; i--) {
+            if (carry == 1) {
+                if (digits[i] == 9) {
+                digits[i] = 0;
+                }
+                else {
+                    digits[i] = digits[i] + 1;
+                    carry = 0;
+                }
+            }
         }
         if (carry == 1) {
             int[] newDigits = new int[digits.length + 1];
             newDigits[0] = 1;
             return newDigits;
         }
-
         return digits;
     }
 }
