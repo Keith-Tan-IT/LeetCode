2c0eaa784945aa2b9375929b422b6e8ccc4cf4e2
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-08-28 16:05:01 +0000

    LeetCode submission: count-complete-tree-nodes

---
+++ submissions/count-complete-tree-nodes/2025-08-28-160501.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        int height = height(root);
+        return height == -1 ? 0 : height(root.right) == height - 1 ?
+        (1 << height) + countNodes(root.right) : 
+        (1 << height - 1) + countNodes(root.left);
+    }
+
+    public int height(TreeNode root) {
+        return root == null ? -1 : 1 + height(root.left);
+    }
+}
