eb4269014641ff590c84bbf5c01e1ec50b2194dc
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-08-29 15:24:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (50.29%)

--- my-folder/0062-unique-paths/solution.java
+++ my-folder/0062-unique-paths/solution.java
@@ -1,19 +1,17 @@
 class Solution {
-    int[][] dp = new int [101][101];
     public int uniquePaths(int m, int n) {
-        return dfs(m, n, 0, 0);
-    }
-    public int dfs(int m, int n, int i, int j) {
-        if (i >= m || j >= n) {
-            return 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i ++) {
+            dp[i][0] = 1;
         }
-        if (i == m - 1 && j == n - 1) {
-            return 1;
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
         }
-        if (dp[i][j] != 0) {
-            return dp[i][j];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
         }
-        dp[i][j] = dfs(m, n, i + 1, j) + dfs(m, n, i, j+1);
-        return dp[i][j];
+        return dp[m - 1][n - 1];
     }
 }
