dc57409d172cdac4c4b1d87823e9315d8045b3bd
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-08-31 13:35:35 +0000

    [LeetCode Sync] Runtime - 2 ms (100.00%), Memory - 42.4 MB (48.27%)

--- my-folder/3309-count-prefix-and-suffix-pairs-i/solution.java
+++ my-folder/3309-count-prefix-and-suffix-pairs-i/solution.java
@@ -1,12 +1,8 @@
 class Solution {
     public int countPrefixSuffixPairs(String[] words) {
         int count = 0;
-
         for (int i = 0; i < words.length - 1; i++) {
             for (int j = i + 1; j < words.length; j++) {
-                if (words[j].length() < words[i].length()) {
-                    continue;
-                }
                 if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
                     count++;
                 }
