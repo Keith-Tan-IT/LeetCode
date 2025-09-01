53aa469f1b0fc8147f868e01e21cd94a5f28b7b0
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-09-01 15:12:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (98.01%)

---
+++ my-folder/0091-decode-ways/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] memo = new int[s.length()];
+        Arrays.fill(memo, -1);
+        if (s.length() == 0) {
+            return 0;
+        }
+        else {
+            return dfs(s, 0, memo);
+        }
+    }
+    public int dfs (String s, int curr, int[] memo) {
+        if (curr == s.length()) {
+            return 1;
+        }
+        if (s.charAt(curr) == '0') {
+            return 0;
+        }
+        if (memo[curr] != -1) {
+            return memo[curr];
+        }
+        int ways = dfs (s, curr + 1, memo);
+        if (curr + 1 < s.length() && (s.charAt(curr) == '1' || s.charAt(curr) == '2' && s.charAt(curr + 1) < '7')) {
+            ways += dfs(s, curr + 2, memo);
+        }
+        memo[curr] = ways;
+        return memo[curr];
+    }
+}
