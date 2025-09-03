7d5939524514c419b329cbe3c037829c481f133b
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-09-03 15:40:06 +0000

    LeetCode submission: add-to-array-form-of-integer

---
+++ submissions/add-to-array-form-of-integer/2025-09-03-154006.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = num.length - 1; i >= 0; i--) {
+            k = num[i] + k;
+           result.add(0, k % 10); 
+            k /= 10;
+        }
+        while (k > 0) {
+            result.add(0, k % 10);
+            k /= 10;
+        }
+        return result;
+    }
+}
