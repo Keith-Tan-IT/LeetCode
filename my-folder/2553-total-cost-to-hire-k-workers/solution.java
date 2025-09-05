020ef17f75decc77084b65a68a0ab23c882160f5
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-09-05 15:12:45 +0000

    [LeetCode Sync] Runtime - 48 ms (47.79%), Memory - 56.6 MB (71.59%)

--- my-folder/2553-total-cost-to-hire-k-workers/solution.java
+++ my-folder/2553-total-cost-to-hire-k-workers/solution.java
@@ -6,21 +6,13 @@ class Solution {
         PriorityQueue<Integer> leftPQ = new PriorityQueue<>();
         PriorityQueue<Integer> rightPQ = new PriorityQueue<>();
 
-        for (int i = 0; i < candidates && left <= right; i++) {
-            if (left == right) {
-                leftPQ.add(costs[left++]);
-                System.out.println(leftPQ);
-
-                break;
+        while (k-- > 0) { 
+            while (leftPQ.size() < candidates && left <= right) {
+                leftPQ.offer(costs[left++]);
             }
-            leftPQ.add(costs[left++]);
-            rightPQ.add(costs[right--]);
-            /*
-            System.out.println(leftPQ);
-            System.out.println(rightPQ);
-            */
-        }
-        while (k > 0) { 
+            while (rightPQ.size() < candidates && left <= right) {
+                rightPQ.offer(costs[right--]);
+            }
             if (rightPQ.isEmpty() || !leftPQ.isEmpty() && leftPQ.peek() <= rightPQ.peek()) {
 
                 total += leftPQ.poll();
@@ -38,7 +30,6 @@ class Solution {
                     rightPQ.add(costs[right--]);
                 }
             }
-            k--;
         }
         return total;
     }
