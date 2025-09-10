a35b63db767efee99022d562192ba64c5c806425
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-09-10 15:08:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.4 MB (39.46%)

--- my-folder/0112-path-sum/solution.java
+++ my-folder/0112-path-sum/solution.java
@@ -18,8 +18,8 @@ class Solution {
         if (root == null) {
             return false;
         }
-        if (root.left == null && root.right == null && targetSum == root.val) {
-            return true;
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
         }
         return hasPathSum(root.left, targetSum - root.val) ||
                 hasPathSum(root.right, targetSum - root.val);
