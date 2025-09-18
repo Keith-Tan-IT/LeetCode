66b68d06dfa373047063c10ed7c3338af2f4de9b
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-09-18 12:54:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (51.84%)

--- my-folder/0091-decode-ways/solution.java
+++ my-folder/0091-decode-ways/solution.java
@@ -1,29 +1,16 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] memo = new int[s.length()];
-        Arrays.fill(memo, -1);
-        if (s.length() == 0) {
-            return 0;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] = dp[i + 1];
+                if (i < n-1 && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7')) {
+                    dp[i] += dp[i + 2];
+                }
+            }
         }
-        else {
-            return dfs(s, 0, memo);
-        }
-    }
-    public int dfs (String s, int curr, int[] memo) {
-        if (curr == s.length()) {
-            return 1;
-        }
-        if (s.charAt(curr) == '0') {
-            return 0;
-        }
-        if (memo[curr] != -1) {
-            return memo[curr];
-        }
-        int ways = dfs (s, curr + 1, memo);
-        if (curr + 1 < s.length() && (s.charAt(curr) == '1' || s.charAt(curr) == '2' && s.charAt(curr + 1) < '7')) {
-            ways += dfs(s, curr + 2, memo);
-        }
-        memo[curr] = ways;
-        return memo[curr];
+        return dp[0];
     }
 }
