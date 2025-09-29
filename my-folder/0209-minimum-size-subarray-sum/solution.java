1880a9cd24af98a50143867009c0dac1e098c7a1
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-09-29 14:50:49 +0000

    [LeetCode Sync] Runtime - 1 ms (99.40%), Memory - 58.3 MB (31.52%)

---
+++ my-folder/0209-minimum-size-subarray-sum/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        int left = 0, right = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum < target) {
+                sum += nums[i];
+            }
+            while(sum >= target) {
+                min = Math.min(min, i - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+}
