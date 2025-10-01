0b6c2a13dbd464199f338685f807053712c0c175
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-10-01 14:48:16 +0000

    [LeetCode Sync] Runtime - 2 ms (31.09%), Memory - 45.2 MB (26.91%)

--- my-folder/0057-insert-interval/solution.java
+++ my-folder/0057-insert-interval/solution.java
@@ -45,19 +45,20 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> result = new ArrayList<>();
-        int i = 0;
-
-        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
-            result.add(intervals[i++]);
-        }
-        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i++][1]);
+        for (int i = 0; i < intervals.length; i++) {
+            if (newInterval[0] > intervals[i][1]) {
+                result.add(intervals[i]);
+            }
+            else if (newInterval[1] >= intervals[i][0]) {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+            else {
+                result.add(newInterval);
+                newInterval = intervals[i];
+            }
         }
         result.add(newInterval);
-        while(i < intervals.length) {
-            result.add(intervals[i++]);
-        }
-        return result.toArray(new int[result.size() - 1][]);
+        return result.toArray(new int[0][]);
     }
 }
