a926fdbdd5c90a8f5cfdb7b56695cdc52923d17d
Keith-Tan-IT <134896109+Keith-Tan-IT@users.noreply.github.com>
2025-10-03 16:20:20 +0000

    [LeetCode Sync] Runtime - 1 ms (96.01%), Memory - 41.7 MB (50.29%)

--- my-folder/2442-number-of-arithmetic-triplets/solution.java
+++ my-folder/2442-number-of-arithmetic-triplets/solution.java
@@ -1,15 +1,20 @@
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
         int counter = 0;
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
-                        counter++;
-                    }
-                }
+        for (int i = 0; i < nums.length; i++) {
+            if (contains(nums, nums[i] + diff) && contains(nums, nums[i] + 2 * diff)) {
+                counter++;
             }
         }
         return counter;
+    }
+
+    public boolean contains(int[] nums, int target) {
+        for (int num : nums) {
+            if (num == target) {
+                return true;
+            }
+        }
+        return false;
     }
 }
