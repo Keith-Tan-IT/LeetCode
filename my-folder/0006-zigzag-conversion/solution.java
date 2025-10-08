a37946cb0c7d995a9555e479d5312423a0857e7b
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-10-08 14:14:48 +0000

    [LeetCode Sync] Runtime - 4 ms (84.74%), Memory - 45.2 MB (45.54%)

--- my-folder/0006-zigzag-conversion/solution.java
+++ my-folder/0006-zigzag-conversion/solution.java
@@ -1,23 +1,21 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1) {
-            return s;
-        }
+        char[] c = s.toCharArray();
         StringBuilder[] sb = new StringBuilder[numRows];
         for (int i = 0; i < numRows; i++) {
             sb[i] = new StringBuilder();
         }
-        boolean goingDown = true;
-        int currRow = 0;
-        for (char c : s.toCharArray()) {
-            sb[currRow].append(c);
-            if (currRow == 0 || currRow == numRows - 1) {
-                goingDown = !goingDown;
+        int i = 0;
+        while (i < s.length()) {
+            for (int idx = 0; idx < numRows && i < s.length(); idx++) {
+                sb[idx].append(c[i++]);
             }
-            currRow += goingDown ? -1 : 1;
+            for (int idx = numRows - 2; idx >= 1 && i < c.length; idx--) {
+                sb[idx].append(c[i++]);
+            }
         }
-        for (int i = 1; i < numRows; i++) {
-            sb[0].append(sb[i]);
+        for (int idx = 1; idx < numRows; idx++) {
+            sb[0].append(sb[idx]);
         }
         return sb[0].toString();
     }
