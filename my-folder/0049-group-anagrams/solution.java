68ac3a2a439703d3c869a734820529b827914871
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-10-09 14:50:15 +0000

    [LeetCode Sync] Runtime - 17 ms (26.17%), Memory - 48.3 MB (24.89%)

--- my-folder/0049-group-anagrams/solution.java
+++ my-folder/0049-group-anagrams/solution.java
@@ -67,6 +67,9 @@
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
             int[] count = new int[26];
