f9337e7c7de9419f62842bdb3f2c3e5dfba0ffc5
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-10-21 11:20:13 +0000

    [LeetCode Sync] Runtime - 23 ms (13.21%), Memory - 41.4 MB (75.14%)

--- my-folder/2442-number-of-arithmetic-triplets/solution.java
+++ my-folder/2442-number-of-arithmetic-triplets/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
-        Map <Integer, Integer> hashMap = new HashMap<>();
-        int count = 0, i = 0;
-        for (int num : nums) {
-            hashMap.put(num, i++);
-        }
-        for (int num : nums) {
-            if (hashMap.containsKey(num + diff) && hashMap.containsKey(num + 2 * diff)) {
-                count++;
+        int counter = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
+                        counter++;
+                    }
+                }
             }
         }
-        return count;
+        return counter;
     }
 }
