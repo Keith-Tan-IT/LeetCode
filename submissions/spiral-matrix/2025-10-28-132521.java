5138adb2403b1030a7e1229f81160932cebea321
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-10-28 13:25:21 +0000

    LeetCode submission: spiral-matrix

---
+++ submissions/spiral-matrix/2025-10-28-132521.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+        while (bottom >= top && right >= left) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int j = top; j <= bottom; j++) {
+                result.add(matrix[j][right]);
+            }
+            right--;
+
+            if (bottom >= top) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (right >= left) {
+                for (int j = bottom; j >= top; j--) {
+                    result.add(matrix[j][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
