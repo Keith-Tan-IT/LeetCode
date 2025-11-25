b5e70c79ccfd6b584c3a29183a80621906cb1079
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-11-25 13:11:30 +0000

    LeetCode submission: container-with-most-water

---
+++ submissions/container-with-most-water/2025-11-25-131130.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            max = Math.max(max, (right - left) * Math.min(height[left], height[right]));
+            if (height[right] > height[left]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
