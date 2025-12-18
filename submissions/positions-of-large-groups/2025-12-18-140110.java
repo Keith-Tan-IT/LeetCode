bf95f1224b052e55a0858c04ef09c2ceb734e4c7
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2025-12-18 14:01:10 +0000

    LeetCode submission: positions-of-large-groups

---
+++ submissions/positions-of-large-groups/2025-12-18-140110.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String s) {
+        List<List<Integer>> result = new ArrayList<>();
+        int start = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i == s.length() - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                int length = i - start + 1;
+                if (length >= 3) {
+                    result.add(Arrays.asList(start, i));
+                }
+                start = i + 1;
+            }
+        }
+        return result;
+    }
+}
