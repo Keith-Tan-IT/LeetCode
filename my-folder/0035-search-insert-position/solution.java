5c749416ba2560f281c0adacf51ee35e89124b71
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2026-02-10 17:56:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.5 MB (91.50%)

--- my-folder/0035-search-insert-position/solution.java
+++ my-folder/0035-search-insert-position/solution.java
@@ -1,16 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length;
-        while (right > left) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
             }
             else {
                 left = mid + 1;
             }
-        }
+        }            
         return left;
+
     }
 }
