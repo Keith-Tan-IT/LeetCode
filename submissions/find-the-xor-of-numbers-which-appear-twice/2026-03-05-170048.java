5f0a0b70d68ba47b234f25f7c5ccc99e0e3ae9bb
Keith-Tan-IT <Keith-Tan-IT@users.noreply.github.com>
2026-03-05 17:00:48 +0000

    LeetCode submission: find-the-xor-of-numbers-which-appear-twice

---
+++ submissions/find-the-xor-of-numbers-which-appear-twice/2026-03-05-170048.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int duplicateNumbersXOR(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                ans ^= nums[i];
+                i++;
+            }
+        }
+        return ans;
+    }
+}
